5fd9c12bc4d7d08b84f9d6106744112ee5b6270a
Karen V <karenvo@usc.edu>
2019-12-11 02:10:57 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :artists, only: [:show] do
-    resources :songs, only: [:show, :index]
+  resources :artists
+  
+  resources :artists do
+    resources :songs, only: [:index, :show]
   end
-  resources :songs, only: [:index, :show, :new, :create, :edit, :update]
 
+  resources :songs
 end
